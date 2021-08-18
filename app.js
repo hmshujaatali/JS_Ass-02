6a69c4311fa6f6698cc0c5f0bc899f2698dd08e1
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:02:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+                //QUSETION#1
+//  var a, b, c;
+                  //QUSETION#2
+                  //legal variable names
+//var dell
+//var _dell
+//var $dell
+//var Dell9
+//var dEll
+                  //illegal variable names
+//var Dell#
+//var .dEll
+//var 0dell
+//var -dell
+//var d-ell
+                  //QUSETION#3
+//document.write("<h1>Rules for naming JS variables</h1><br/><br/>")
+//document.write("Variable names can only contain , numbers,$ and ... . For example : $my_1stVariable<br/>")
+//document.write("Variable must begin with a letter, $ or _. For example $name, _name or name<br/>") 
+//document.write("Variable names are case sensitive<br/>")
+//document.write(" Variable names should not be JS Keywords")
+
