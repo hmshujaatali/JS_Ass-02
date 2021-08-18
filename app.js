a1e89ee8996deaa450ff3962902838b266c27c17
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:03:37 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,122 @@
                 //QUSETION#1
-//  var a, b, c;
-                  //QUSETION#2
-                  //legal variable names
-//var dell
-//var _dell
-//var $dell
-//var Dell9
-//var dEll
-                  //illegal variable names
-//var Dell#
-//var .dEll
-//var 0dell
-//var -dell
-//var d-ell
-                  //QUSETION#3
-//document.write("<h1>Rules for naming JS variables</h1><br/><br/>")
-//document.write("Variable names can only contain , numbers,$ and ... . For example : $my_1stVariable<br/>")
-//document.write("Variable must begin with a letter, $ or _. For example $name, _name or name<br/>") 
-//document.write("Variable names are case sensitive<br/>")
-//document.write(" Variable names should not be JS Keywords")
-
+//var a=+prompt("enter 1st number")
+//var b=+prompt("enter 2nd number")
+//c=a+b
+//document.write(c)
+                //QUSETION#2
+                //(subtraction)
+//var a=+prompt("enter 1st number")
+//var b=+prompt("enter 2nd number")
+//c=a-b
+//document.write(c)
+                //(Multiplication)
+//var a=+prompt("enter 1st number")
+//var b=+prompt("enter 2nd number")
+//c=a*b
+//document.write(c)
+                //(Division)
+//var a=+prompt("enter 1st number")
+//var b=+prompt("enter 2nd number")
+//c=a/b
+//document.write(c)
+                //QUSETION#3
+//var a 
+//document.write("value after variable declaration is:<br/>",a)
+//a=5
+//document.write("initial value:",a,"<br/>")
+//a++
+//document.write("Value after increment is:",a,"<br/>")
+//a=a+7
+//document.write("Value after increment is:",a,"<br/>")
+//a=--a
+//document.write("Value after increment is:",a,"<br/>")
+//a=a/3
+//a=a-a
+//document.write("The Reminder is:",a,"<br/>")
+                //QUSETION#4
+//var oneMovieTicket=600
+//var numBerofTicket=+prompt("enter required no.of tickets:")
+//a=numBerofTicket*oneMovieTicket
+//document.write("the cost of buying",numBerofTicket,"to a movie is:",a,"PKR")
+                //QUSETION#5
+//document.write("Table of 4<br/>")
+//document.write("4x1=4<br/>")
+//document.write("4x2=8<br/>")
+//document.write("4x3=12<br/>")
+//document.write("4x4=16<br/>")
+//document.write("4x5=20<br/>")
+//document.write("4x6=24<br/>")
+//document.write("4x7=28<br/>")
+//document.write("4x8=32<br/>")
+//document.write("4x9=36<br/>")
+//document.write("4x10=40<br/>")
+                //QUSETION#6
+// var c=25
+// f=(c*9/5)+32
+// document.write("25oC is",f,"oF")
+// var f=70
+// c=(f-32)*5/9
+// document.write("<br/>77oC is",c,"oF")
+                //QUSETION#7
+// var priceOfitem1=60
+// var priceOfitem2=100
+// var orderQuantityitem1=3
+// var orderQuantityitem2=7
+// var shippingCharges=100
+// var tOtal
+// tOtal=priceOfitem1+priceOfitem2+orderQuantityitem1+orderQuantityitem2+shippingCharges
+// document.write("Price of item 1 is ",priceOfitem1)
+// document.write("<br/>Quantity of item 1 is ",orderQuantityitem1)
+// document.write("<br/>Price of item 2 is ",priceOfitem2)
+// document.write("<br/>Quantity of item 2 is ",orderQuantityitem2)
+// document.write("<br/>Shipping Charges ",shippingCharges)
+// document.write("<br/><br/>Total cost of your order is ",tOtal)
+                //QUSETION#8
+// var totalMarks=980
+// var marksObtained=804
+// var percentage
+// percentage=(marksObtained/totalMarks)*100
+// document.write("Total marks: ",totalMarks)
+// document.write("<br/>Marks Obtained: ",marksObtained)
+// document.write("<br/>Percentage: ",percentage)
+                //QUSETION#9
+// var usd=10
+// var sr=25
+// var PKR
+// PKR=(usd*104.80)+(sr*28)
+// document.write("Total Currency in PKR:",PKR)
+                //QUSETION#10
+// var num1=10
+// num2=((num1+5)*10)/2
+// document.write(num2)
+                //QUSETION#11
+// var cy=2016
+// var by=1992
+// var yage
+// yage=cy-by
+// document.write("Current Year: ",cy)
+// document.write("<br/>Birth Year: ",by)
+// document.write("<br/>Your Age is: ",yage)
+                //QUSETION#12
+// var radius=20
+// var circumference
+// var pi=3.142
+// var area
+// area=pi*radius*radius
+// circumference=2*pi*radius
+// document.write("Radius of a circle: ",radius)
+// document.write("<br/>The circumference is: ",circumference) 
+// document.write("<br/>The are is: ",area)             
+                //QUSETION#13
+// var fs="chocolate Chip"
+// var ca=15
+// var ema=65
+// var aOspd=3
+// var req
+// req=(ema-ca)*aOspd
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// document.write("<br/>Favourite Snack: ",fs)
+// document.write("<br/>Current age: ",ca)
+// document.write("<br/>Estimated Maximum Age: ",ema)
+// document.write("<br/>Amount of snacks per day:",aOspd)
+// document.write("<br/>You will need ",req," to last you until the ripe old age of ",ema)
